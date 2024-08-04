6166efc071ec5653f403c37cb2066c340f0efdfb
FrankRoos <frankroos.fn@gmail.com>
2024-08-04 16:49:19 +0200

    Update logo and existing link

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
-git add .
-git commit -m "First commit: init react app"
-gh repo create forage-propeller-github-cli --public --source=. --remote=upstream
-gh repo sync
+cd src/
+rm logo.svg
+# https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg return 404
+# Download the logo from the propeller website and save it in the src directory
+wget https://cdn-ikponof.nitrocdn.com/vGqfYAGlOLDkYkJqZhYIYKEsibdbZnkc/assets/images/source/rev-c0d692a/www.propelleraero.com/wp-content/uploads/2023/05/logo.svg
+cd ../
